8906037e510586deb8d0b3ddae490176c5bf0e48
Filipe <fmartns66@gmail.com>
2026-01-11 00:29:48 -0300

    feat: adicionar STATICFILES_DIRS nas configurações para gerenciamento de arquivos estáticos (#1)

--- config/settings.py
+++ config/settings.py
@@ -146,6 +146,8 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = "static/"
